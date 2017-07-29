d647f7afd06af9985cd5a79d051ab09f45ddfb2a
Ben <ben@govhack>
2017-07-30 00:16:29 +1000

    geo checking api working

--- ServerApp/app.py
+++ ServerApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, send_from_directory
+from flask import Flask, render_template, url_for, redirect, send_from_directory, request, jsonify
 from helpers import *
 
 app = Flask(__name__)
@@ -8,12 +8,18 @@ def send_file(path):
     return send_from_directory('', path)
 
 @app.route('/warning')
-def warning(lat=0, long=0):
-    danger = zone_checker(lat, long)
-
+def warning():
     return render_template("warning.html")
 
-#    if danger == True:
-#        return 'Danger ' + str(lat) + str(long)
-#    else:
-#        return 'Safe'
+@app.route('/check')
+def check_geo():
+    lat = float(request.args.get('lat'))
+    lon =float(request.args.get('lon'))
+
+    danger = zone_checker(lat, lon)
+
+    return jsonify(
+        lat = lat,
+        lon = lon,
+        blackspot = danger
+    )
--- ServerApp/helpers.py
+++ ServerApp/helpers.py
@@ -1,4 +1,28 @@
+import json
+from geojson_utils import point_in_multipolygon
+from geojson import Point
 
-def zone_checker(lat, long):
-    warning = True
-    return warning
+def zone_checker(lat, lon):
+    # load danger zones multipolygon file
+    blackspot_file = 'dangerzones.json' # danger zones
+    #blackspot_file = 'canberra.geojson' # for testing
+    #blackspot_file = 'switzerland.geojson' # for testing
+
+    file = open(blackspot_file, 'r')
+    zones_str = file.read()
+    file.close()
+
+    # convert data to json
+    coord_json = Point((lat, lon))  #coordinates from function args
+    zones_json = json.loads(zones_str) #zone shapes from file
+
+    # determine if coordinates location lies in multiPolygon zone data
+    zone = point_in_multipolygon(coord_json, zones_json)
+
+    # Return True if coordinates are within multipolygon zones file
+    return zone
+
+if __name__ == "__main__":
+    # use test coordinates
+    test = zone_checker(149.141787, -35.311173)
+    print(test)
